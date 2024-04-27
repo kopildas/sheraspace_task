6c7ae0cd080c6c8021b1e49c375e0bed76d62223
kopildas <102140363+kopildas@users.noreply.github.com>
2024-04-28 05:06:41 +0600

    feat: skeleton animation added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulse: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+      keyframes: {
+        pulse: {
+          "0%": {
+            opacity: "1",
+          },
+          "50%": {
+            opacity: ".5",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
