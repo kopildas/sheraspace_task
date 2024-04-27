6b1519ccc077d974f95dd8858c2fd7fa5b67378e
kopildas <102140363+kopildas@users.noreply.github.com>
2024-04-28 03:36:13 +0600

    fix: "connection already closed" error & refactor

--- server/app.py
+++ server/app.py
@@ -5,104 +5,72 @@ from flask_cors import CORS
 import psycopg2
 from dotenv import load_dotenv
 
-
 load_dotenv()
-
 
 app = Flask(__name__)
 cors = CORS(app, origins='http://localhost:5173')
 
-# Initialize spaCy English language model. Due to low memory in production i use small model
+# Initialize spaCy English language model
 nlp = spacy.load("en_core_web_sm")
-
-
-
 
 # Function to preprocess text using spaCy
 def preprocess_text(text):
-    doc = nlp(text.lower())  
-    tokens = [token.text for token in doc if not token.is_stop and not token.is_punct]  # Remove stop words and punctuation
-    # Join the tokens with a space to form a string
+    doc = nlp(text.lower())
+    tokens = [token.text for token in doc if not token.is_stop and not token.is_punct]
     preprocessed_text = ' '.join(tokens)
     return preprocessed_text
 
-
-
-
 # Function to find the closest matching answer using NLP
-def find_most_similar_question(user_question, database_questions):
-    # Preprocess the user question
+def find_most_similar_question(user_question):
     user_question_processed = preprocess_text(user_question)
-
-    # variables for tracking the best match
     best_match_score = 0
     best_match_question = None
+    connection,database_questions = get_db_connection()
 
-    # Loop through each database question
     for question in database_questions:
-        # Preprocess the database question
         question_processed = preprocess_text(question)
-
-        # Calculate similarity between the user question and the current database question
         user_doc = nlp(user_question_processed)
         question_doc = nlp(question_processed)
         similarity = user_doc.similarity(question_doc)
 
-        # Update the best match if the current similarity is higher
         if similarity > best_match_score:
             best_match_score = similarity
             best_match_question = question
 
-    # Check if a close enough match was found
     if best_match_score < 0.5:
         return None, None
 
-    try:
-        cursor = connection.cursor()
-        cursor.execute("SELECT answer FROM questions_answer WHERE question = %s", (best_match_question,))
-        answer = cursor.fetchone()[0]
-        # connection.commit()
-    except (Exception, psycopg2.Error) as error:
-        print("Error getting best_match_question data from DB: ", error)
-    finally:
-        cursor.close()
+    cursor = connection.cursor()
+    cursor.execute("SELECT answer FROM questions_answer WHERE question = %s", (best_match_question,))
+    answer = cursor.fetchone()[0]
+    cursor.close()
+    connection.close()
 
     return answer, best_match_score
-
-
-
 
 # Database connection object (initially None)
 connection = None
 database_questions = None
-cursor = None
 
 # Database connection function
 def get_db_connection():
     global connection
     global database_questions
-    global cursor
 
-    if connection is None:
-        print("db connect")
+    if connection is None or database_questions is None:
         try:
             db_url = os.getenv("DATABASE_URL")
             connection = psycopg2.connect(db_url)
-            cursor = connection.cursor()
-            try:
+
+            if database_questions is None:
+                cursor = connection.cursor()
                 cursor.execute("SELECT question FROM questions_answer")
-                database_questions = [row[0] for row in cursor.fetchall()] 
-                # connection.commit()
-            except (Exception, psycopg2.Error) as error:
-                print("Error getting data from DB: ", error)
-            finally:
+                database_questions = [row[0] for row in cursor.fetchall()]
                 cursor.close()
         except Exception as e:
             print(f"Error connecting to database: {e}")
             exit(1)
     return connection,database_questions
-
-
 
 @app.post("/api/answer")
 def answer_question():
@@ -110,18 +78,12 @@ def answer_question():
         data = request.get_json()
         question = data.get("question")
         if question:
-            # Connecting to the database
-            connection, database_questions = get_db_connection()
-
             try:
-                # Finding the most similar answer
-                answer, similarity = find_most_similar_question(question, database_questions)
-
+                answer, similarity = find_most_similar_question(question)
                 if answer:
                     return jsonify({"answer": answer, "similarity": similarity})
                 else:
                     return jsonify({"answer": "Your question is not relatable to our service."})
-
             except Exception as e:
                 print(f"Error accessing database: {e}")
                 return jsonify({"error": "Database error occurred."}), 500
@@ -129,8 +91,6 @@ def answer_question():
             return jsonify({"error": "Missing required field 'question'"}), 400
     else:
         return jsonify({"error": "Unsupported Media Type. Please use JSON."}), 415
-
-
 
 @app.get("/")
 def index():
